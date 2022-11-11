95dda89c5b0e3a867bf62064e9a993a5756b323e
BatryCC <dianchimxqpx@126.com>
2022-11-11 08:35:39 +0800

    感谢Superice666的帮助捏

--- src/Runner.go
+++ src/Runner.go
@@ -1,44 +1,50 @@
 package src
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"log"
+	//"bufio"
+	//"fmt"
+	//"io"
+	//"log"
 	"os/exec"
-
-	"fyne.io/fyne/v2/widget"
+	//"fyne.io/fyne/v2/widget"
 	//"fyne.io/fyne/widget"
 	//"fyne.io/fyne/v2"
 	//"fyne.io/fyne/v2/widget"
 )
 
-func Runner(entry *widget.Entry, startcommand string) (io.ReadCloser, *bufio.Writer) {
-	cmd := exec.Command(startcommand)
-
-	stdout, err := cmd.StdoutPipe()
+func Runner(startcommand string) {
+	cmd := exec.Command("cmd.exe", "/c", "start "+startcommand)
+	err := cmd.Run()
 	if err != nil {
-		log.Fatalln(err)
-	}
-	stdin, err := cmd.StdinPipe()
-	if err != nil {
-		log.Fatalln(err)
+		panic(err)
 	}
 
-	//执行命令
-	if err := cmd.Start(); err != nil {
-		log.Fatalln(err)
-	}
-	readerr := bufio.NewReader(stdout)
-	writer := bufio.NewWriter(stdin)
-	go func() {
-		GetOutput(readerr, entry)
-	}()
-	//writer.Write()
-	return stdout, writer
+	/*
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		stdin, err := cmd.StdinPipe()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		//执行命令
+		if err := cmd.Start(); err != nil {
+			log.Fatalln(err)
+		}
+		readerr := bufio.NewReader(stdout)
+		writer := bufio.NewWriter(stdin)
+		go func() {
+			GetOutput(readerr)
+		}()
+		//writer.Write()
+		return stdout, writer
+	*/
 }
 
-func GetOutput(reader *bufio.Reader, entry *widget.Entry) string {
+/*
+func GetOutput(reader *bufio.Reader) string {
 	var sumOutput string
 	outputBytes := make([]byte, 200)
 	for {
@@ -54,12 +60,8 @@ func GetOutput(reader *bufio.Reader, entry *widget.Entry) string {
 
 		//fmt.Print(output) //输出屏幕内容
 		sumOutput += output
-		entry.SetText(sumOutput)
-		//entry.Refresh()
 	}
 	return sumOutput
 }
 
-func JavaParser() {
-
-}
+*/
--- ui/SettingMenu.go
+++ ui/SettingMenu.go
@@ -1,1 +1,11 @@
 package ui
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+)
+
+func SettingMenu(w fyne.Window) *fyne.Container {
+	settingmenu := container.NewVBox()
+	return settingmenu
+}
